5dd33f0e1b59d8aeb8d3a53461c0ddd852a4ee91
Hasensab <r161135@rguktrkv.ac.in>
2023-11-29 18:46:46 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bts4(request):
+    return render(request,'bts4.html')
+def carousel(request):
+    return render(request,'carousel.html')
